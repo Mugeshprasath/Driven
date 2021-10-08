b2872926bb840006614f62b13a5120d25b50ac62
Mugeshprasath <mugeshprasath7871@gmail.com>
2021-10-08 09:34:13 -0700

    zzzzzz

--- src/org/nnnnnnn/MMMMM.java
+++ src/org/nnnnnnn/MMMMM.java
@@ -4,4 +4,7 @@ public class MMMMM {
 
 public static void main(String[] args) {
 	System.out.println("jjjjjjjjj");
+	System.out.println("jjjjjjjjj");
+	System.out.println("jjjjjjjjj");
+	System.out.println("jjjjjjjjj");
 }}
